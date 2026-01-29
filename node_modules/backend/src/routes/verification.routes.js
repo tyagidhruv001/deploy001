321d9c83bdec2ddf6758191f1ffff3c808d0adc8
tyagidhruv001 <dhruvtyagi229@gmail.com>
2026-01-29 14:39:21 +0530

    chore: clean up node_modules and update gitignore

--- node_modules/backend/src/routes/verification.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const verificationController = require('../controllers/verification.controller');
-
-router.post('/verify', verificationController.verify);
-
-module.exports = router;
