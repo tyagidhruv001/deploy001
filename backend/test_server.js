42b6dbe4fd70761535627d0f19a29e189ce09aa4
tyagidhruv001 <dhruvtyagi229@gmail.com>
2026-01-29 08:27:40 +0530

    chore: cleanup debug scripts and final production build

--- backend/test_server.js
+++
@@ -1,6 +0,0 @@
-
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('ok'));
-app.listen(5000, () => console.log('Test server on 5000'));
-console.log('Script end reached');
