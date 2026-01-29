5c8d6ae23a7913ace34b1415c7eb59a6a02fef6e
tyagidhruv001 <dhruvtyagi229@gmail.com>
2026-01-29 21:04:35 +0530

    chore: clean up project structure, ignore node_modules and secrets

--- backend/src/middlewares/error.middleware.js
+++
@@ -1,8 +0,0 @@
-module.exports = (err, req, res, next) => {
-    console.error(err);
-
-    res.status(err.statusCode || 500).json({
-        success: false,
-        message: err.message || 'Internal Server Error'
-    });
-};
